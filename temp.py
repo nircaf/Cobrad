1562d1b20f8a4eb20b0c068f52d8fe4011435a8f
Nir Cafri <nircafri@quanthealth.ai>
2025-03-20 15:39:24 +0200

    a

--- temp.py
+++ temp.py
@@ -5,10 +5,10 @@ directory = 'temps_EDF'
 
 # Iterate over all files in the directory
 for filename in os.listdir(directory):
-    # Check if the filename contains the word "segment"
-    if 'segment' in filename:
-        # Create the new filename by replacing "segment" with "3600"
-        new_filename = filename.replace('segment', '3600')
+    # Check if the filename contains 'B N,I'
+    if 'B N,I ' in filename:
+        # Create the new filename by removing 'B N,I '
+        new_filename = filename.replace('B N,I ', '')
         # Get the full paths for the old and new filenames
         old_file = os.path.join(directory, filename)
         new_file = os.path.join(directory, new_filename)
